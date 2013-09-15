f9f655077f8fe192235a8b8996c73f1ef4047d99
Viktor Kukurba <victor.kukurba@sethq.com>
2013-09-15 21:26:24 +0300

    test sammy js example

--- sammyjs/tab-nav.js
+++ sammyjs/tab-nav.js
@@ -2,67 +2,74 @@
 
     this.element_selector = '#tab-container';
 
-    this.get('#:id/tab', function (context) {
-        karpaty = karpaty || new FcKarpatyWidget($('<div><div/>', { id: 'karpaty-contant' }).appendTo($(sammy.element_selector)));
+    this.get('#:id/karpaty', function (context) {
+        karpaty = karpaty || new FcKarpatyWidget($('<div/>', { id: 'karpaty-contant'}).addClass('tab-content').appendTo($(sammy.element_selector)));
         if (dnipro) dnipro.hide();
         if (dynamo) dynamo.hide();
         karpaty.show();
-        //$('#tab-content').html('<h1>Tab-' + context['params']['id'] + '</h1>');
-
     });
-    this.get('#:id/tab2', function (context) {
-        dnipro = dnipro || new FcDniproWidget($('<div><div/>', { id: 'dnipro-contant' }).appendTo($(sammy.element_selector)));
+    this.get('#:id/dnipro', function (context) {
+        dnipro = dnipro || new FcDniproWidget($('<div/>', { id: 'dnipro-contant' }).addClass('tab-content').appendTo($(sammy.element_selector)));
         if (karpaty) karpaty.hide();
         if (dynamo) dynamo.hide();
         dnipro.show();
-        //$('#tab-content').html('<h1>Tab2-' + context['params']['id'] + '</h1>');
     });
 
-    this.get('#:id/tab3', function (context) {
-        dynamo = dynamo || new FcDniproWidget($('<div><div/>', { id: 'dynamo-contant' }).appendTo($(sammy.element_selector)));
+    this.get('#:id/dynamo', function (context) {
+        dynamo = dynamo || new FcDynamoWidget($('<div/>', { id: 'dynamo-contant' }).addClass('tab-content').appendTo($(sammy.element_selector)));
         if (karpaty) karpaty.hide();
         if (dnipro) dnipro.hide();
         dynamo.show();
-        //$('#tab-content').html('<h1>Tab3-' + context['params']['id'] + '</h1>');
     });
 
     var sammy = this;
 });
 window.onload = function () {
-    $('#tab-container li').on('click', function () {
-        $('#tab-container li').removeClass('active');
-        $(this).addClass('active');
-    });
+    new TabContainer([{ name: 'dynamo', title: 'Dynamo' }, { name: 'dnipro', title: 'Dnipro' }, { name: 'karpaty', title: 'Karpaty'}]);
     hashRouter.run();
 };
 
 var karpaty, dnipro, dynamo;
 
+function TabContainer(tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+        var li = $('<li class="active"><a href="../sammyjs/index.htm#1/' + tabs[i]['name'] + '">' + tabs[i]['title'] + '</a></li>').appendTo($('ul.tab-menu'));
+        li.on('click', function() {
+            $('#tab-container li').removeClass('active');
+            $(this).addClass('active');
+        });
+    }
+}
+
 function FcWidget($container) {
-    var container = $container;
-    this.show = function() {
-        container.show();
+    this.show = function () {
+        this.container.addClass('tab-content');
+        this.container.show();
     };
 
     this.hide = function () {
-        container.show();
+        this.container.removeClass('tab-content');
+        this.container.hide();
     };
+    
+
+    this.container = $container;
 };
 
-function FcKarpatyWidget($container) {
+function FcKarpatyWidget() {
     FcWidget.apply(this, arguments);
-    $container.append('<h1>FC Karpaty Lviv<h1>');
-    $container.append('<img src="../img/fc_karpaty.gif"/>');
+    this.container.append('<h1>FC Karpaty Lviv<h1>');
+    this.container.append('<img src="../img/fc_karpaty.gif"/>');
 }
 
-function FcDniproWidget($container) {
+function FcDniproWidget() {
     FcWidget.apply(this, arguments);
-    $container.append('<h1>FC Dnipro Dnipropetrovsk<h1>');
-    $container.append('<img src="../img/fc_karpaty.gif"/>');
+    this.container.append('<h1>FC Dnipro Dnipropetrovsk<h1>');
+    this.container.append('<img src="../img/dnipro.gif"/>');
 }
 
-function FcDynamoWidget($container) {
+function FcDynamoWidget() {
     FcWidget.apply(this, arguments);
-    $container.append('<h1>FC Dynamo Kyiv<h1>');
-    $container.append('<img src="../img/fc_karpaty.gif"/>');
+    this.container.append('<h1>FC Dynamo Kyiv<h1>');
+    this.container.append('<img src="../img/fc_dynamo.gif"/>');
 }
