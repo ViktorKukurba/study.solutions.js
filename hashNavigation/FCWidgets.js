d1ef179ab52b746c761f9e6f48c47b435bd0e3dc
Viktor Kukurba <victor.kukurba@sethq.com>
2013-09-18 14:19:54 +0300

    Added simple hash navigation

---
+++ hashNavigation/FCWidgets.js
@@ -0,0 +1,51 @@
+﻿function TabContainer(tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+        var li = $('<li class="active"><a href="../sammyjs/index.htm#1/' + tabs[i]['name'] + '">' + tabs[i]['title'] + '</a></li>').appendTo($('ul.tab-menu'));
+        li.on('click', function () {
+            $('#tab-container li').removeClass('active');
+            $(this).addClass('active');
+        });
+    }
+}
+
+function FcWidget($container) {
+    this.show = function () {
+        this.container.addClass('tab-content');
+        this.container.show();
+    };
+
+    this.hide = function () {
+        this.container.removeClass('tab-content');
+        this.container.hide();
+    };
+
+
+    this.container = $container;
+};
+
+function FcKarpatyWidget() {
+    FcWidget.apply(this, arguments);
+    this.container.append('<h1>FC Karpaty Lviv<h1>');
+    var img = $('<img src="../img/fc_karpaty.gif"/>').appendTo(this.container);
+    img.on('click', function () {
+        alert('Karpaty Logo !');
+    });
+}
+
+function FcDniproWidget() {
+    FcWidget.apply(this, arguments);
+    this.container.append('<h1>FC Dnipro Dnipropetrovsk<h1>');
+    var img = $('<img src="../img/dnipro.gif"/>').appendTo(this.container);
+    img.on('click', function () {
+        alert('Dnipro Logo !');
+    });
+}
+
+function FcDynamoWidget() {
+    FcWidget.apply(this, arguments);
+    this.container.append('<h1>FC Dynamo Kyiv<h1>');
+    var img = $('<img src="../img/fc_dynamo.gif"/>').appendTo(this.container);
+    img.on('click', function () {
+        alert('Dynamo Logo !');
+    });
+}
---
+++ hashNavigation/tab-nav.js
@@ -0,0 +1,86 @@
+﻿
+var hashRouter = (function () {
+    /**
+    * @private
+    * @type {Array}
+    */
+    var routes = [];
+
+    /**
+    * @param {string} route
+    * @return {Object} Route object
+    */
+    var parseRoute = function (route) {
+        /**@type{Object}*/
+        var properties = { route: [], params: {} };
+        /**@type{Array.<string>}*/
+        var routeParams = route['route'].split('/');
+        /**@type{Array.<string>}*/
+        var hashParams = location.hash.replace('#', '').split('/');
+        if (routeParams.length !== hashParams.length) return null;
+        for (/**@type{number} */ var i = 0; i < routeParams.length; i++) {
+            if (routeParams[i].indexOf(':') !== -1) {
+                properties['params'][routeParams[i].replace(':', '')] = hashParams[i];
+            } else if (routeParams[i] === hashParams[i]) {
+                properties['route'].push(hashParams[i]);
+            } else {
+                return null;
+            }
+        }
+        return properties;
+    };
+
+    /**
+    * Handle hash change
+    */
+    var handleHashChange = function () {
+        for (/**@type{number} */ var i = 0; i < routes.length; i++) {
+            var properties = parseRoute(routes[i]);
+            if (!properties) continue;
+            routes[i].callback(properties);
+            return;
+        }
+    };
+    return {
+        run: function () {
+            window.removeEventListener("hashchange", handleHashChange, false);
+            window.addEventListener("hashchange", handleHashChange, false);
+            handleHashChange();
+        },
+        addHashRoute: function (route, callback) {
+            if (typeof route !== 'string' || typeof callback !== 'function') {
+                return;
+            }
+            routes.push({ route: route, callback: callback });
+        }
+    };
+})();
+
+
+window.onload = function () {
+    new TabContainer([{ name: 'dynamo', title: 'Dynamo' }, { name: 'dnipro', title: 'Dnipro' }, { name: 'karpaty', title: 'Karpaty' }]);
+    hashRouter.addHashRoute(':id/karpaty', function (context) {
+        karpaty = karpaty || new FcKarpatyWidget($('<div/>', { id: 'karpaty-contant' }).addClass('tab-content').appendTo($("#tab-container")));
+        if (dnipro) dnipro.hide();
+        if (dynamo) dynamo.hide();
+        karpaty.show();
+    });
+
+    hashRouter.addHashRoute(':id/dnipro', function (context) {
+        dnipro = dnipro || new FcDniproWidget($('<div/>', { id: 'dnipro-contant' }).addClass('tab-content').appendTo($("#tab-container")));
+        if (karpaty) karpaty.hide();
+        if (dynamo) dynamo.hide();
+        dnipro.show();
+    });
+
+    hashRouter.addHashRoute(':id/dynamo', function (context) {
+        dynamo = dynamo || new FcDynamoWidget($('<div/>', { id: 'dynamo-contant' }).addClass('tab-content').appendTo($("#tab-container")));
+        if (karpaty) karpaty.hide();
+        if (dnipro) dnipro.hide();
+        dynamo.show();
+    });
+
+    hashRouter.run();
+};
+
+var karpaty, dnipro, dynamo;
