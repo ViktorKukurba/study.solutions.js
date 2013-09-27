ff37ef6427defbe42ffa63f07bbdf1a60674e38d
Viktor Kukurba <vkuku@i.ua>
2013-09-27 18:05:22 +0300

    added xss test-encoder

---
+++ jquery.plugin/click_dblclick_slowdblclick.js
@@ -0,0 +1,35 @@
+﻿
+/**
+* JQuery extention for handling click, dblclick, slowdblclick
+* @param {function(event):event} clickCallback. Callback for click
+* @param {function(event):event} dblclickCallback. Callback for double click
+* @param {function(event):event} slowDoubleclickCallback. Callback for slow double click
+* @param {number} timout
+* @return {void}
+*/
+jQuery.fn.click_dblclick_slowdblclick = function (clickCallback, dblclickCallback, slowDoubleclickCallback, timeout) {
+    return this.each(function () {
+        var /**@type{number}*/clicks = 0, self = this;
+        jQuery(this).off('click');
+        jQuery(this).on('click', function (event) {
+            clicks++;
+            if (clicks == 1) {
+                setTimeout(function () {
+                    if (clicks == 1) {
+                        setTimeout(function () {
+                            if (clicks == 1) {
+                                clickCallback.call(self, event);
+                            } else {
+                                slowDoubleclickCallback.call(self, event);
+                            }
+                            clicks = 0;
+                        }, 600);
+                    } else {
+                        dblclickCallback.call(self, event);
+                        clicks = 0;
+                    }
+                }, timeout || 300);
+            }
+        });
+    });
+}
---
+++ ui-xss/test-encoder.js
@@ -0,0 +1,10 @@
+﻿var JSON_DATA = {
+    image: 'Text<img src="resources/images/icon-models.png" style="width: 25px;" title="Models">'
+};
+
+$(document).ready(function () {
+    console.log($.encoder);
+    var $body = $('body');
+    $('<div></div>').appendTo($body)[0].innerHTML = JSON_DATA.image;
+    $('<div></div>').appendTo($body)[0].innerHTML = $.encodeForHTML(JSON_DATA.image);
+})
