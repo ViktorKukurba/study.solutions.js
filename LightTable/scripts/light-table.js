a15d7caf7b7799cac89a9f7236a6e2ade7c535bc
Viktor Kukurba <victor.kukurba@sethq.com>
2013-12-15 00:31:50 +0200

    basic sorting added

--- LightTable/scripts/light-table.js
+++ LightTable/scripts/light-table.js
@@ -5,10 +5,11 @@
 /**
  * @param {Element|Node} element Container for table.
  * @param {Object} config Configuration object.
+ * @return {Object} LightTable object.
  * */
-var tableFactory = function (element, config) {
+var tableFactory = function(element, config) {
 
-    var initialize_ = function () {
+    var initialize_ = function() {
         table_ = element_.getElementsByTagName('TABLE')[0];
         if (!table_) {
             table_ = '<table class="ui-table">' +
@@ -24,14 +25,77 @@ var tableFactory = function (element, config) {
     /**
      * @param {Array.<Object>} data Data list.
      * */
-    var draw_ = function (data) {
+    var draw_ = function(data) {
         data_ = data;
         clearTableData_();
         drawHeader_();
         drawTable_();
-        console.log(element_);
-        element_.querySelector('span.desc-sort').onclick = function(){
-            element_.querySelectorAll('tr td:nth-of-type(3)').sort();
+        setSorting_();
+
+    };
+
+    function hasClass(className) {
+        /** @type {!RegExp} */
+        var classNameRe = new RegExp('(?:^|\\s)' + className + '(?!\\S)');
+        return classNameRe.test(this.className);
+    }
+
+    var setSorting_ = function() {
+        /**@type {NodeList}*/
+        var sortedTh = element_.querySelectorAll('span.sort');
+        /** @type {number} */
+        var length = sortedTh.length;
+        if (length > 0)
+        for (/** @type {number} */var index = 0; index < length;)
+            sortedTh[index++].onclick = function() {
+                /**@type {Node|Element}*/ var th = this.parentNode.parentNode;
+                /**@type{string}*/
+                var sortDirection = hasClass.call(this, 'desc-sort') ? '-desc' : '-asc';
+                /**@type {NodeList}*/
+                var trs = element_.querySelectorAll('tbody tr');
+                /** @type {Array<Node|Element>} */
+                var array = Array.prototype.slice.call(trs, 0);
+                /** @type {number} */
+                var columnNumber = parseInt(th.getAttribute('data-column-number'));
+                array.sort(function(a, b) {
+                    return comparer_[settings_['columns'][columnNumber]['type'] + sortDirection](a, b, columnNumber + 1);
+                });
+                /** @type {number} */ var length = array.length;
+                /** @type {number} */ var i = 0;
+                for (; i < length;) {
+                    element_.querySelector('tbody').appendChild(array[i++]);
+                }
+                if (sortDirection === '-desc') {
+                    this.className = this.className.replace('desc-sort', 'asc-sort');
+                } else {
+                    this.className = this.className.replace('asc-sort', 'desc-sort');
+                }
+            };
+    };
+
+    var comparer_ = {
+        'number-desc': function(a, b, columnIndex) {
+            var tda = a.querySelector('tr td:nth-of-type(' + columnIndex + ')');
+            var tdb = b.querySelector('tr td:nth-of-type(' + columnIndex + ')');
+            return parseFloat(tdb.innerText) >= parseFloat(tda.innerText);
+        },
+        'number-asc': function(a, b, columnIndex) {
+            console.log(a, b);
+            var tda = a.querySelector('tr td:nth-of-type(' + columnIndex + ')');
+            var tdb = b.querySelector('tr td:nth-of-type(' + columnIndex + ')');
+            return parseFloat(tda.innerText) >= parseFloat(tdb.innerText);
+        },
+        'string-desc': function(a, b, columnIndex) {
+            console.log(a, b);
+            var tda = a.querySelector('tr td:nth-of-type(' + columnIndex + ')');
+            var tdb = b.querySelector('tr td:nth-of-type(' + columnIndex + ')');
+            return tdb.innerText >= tda.innerText;
+        },
+        'string-asc': function(a, b, columnIndex) {
+            console.log(a, b);
+            var tda = a.querySelector('tr td:nth-of-type(' + columnIndex + ')');
+            var tdb = b.querySelector('tr td:nth-of-type(' + columnIndex + ')');
+            return tda.innerText >= tdb.innerText;
         }
     };
 
@@ -60,15 +124,16 @@ var tableFactory = function (element, config) {
             if (column.sort) {
                 th.setAttribute('data-column', column.name);
                 th.setAttribute('data-sorting', column['sort']);
+                th.setAttribute('data-column-number', index);
                 var span = div.appendChild(document.createElement('span'));
-                span.className = 'desc-sort';
+                span.className = 'sort desc-sort';
                 span.innerHTML = 'd';
             }
         }
     }
 
     function drawTable_() {
-        addRows_(data_)
+        addRows_(data_);
     }
 
     /**
@@ -96,7 +161,6 @@ var tableFactory = function (element, config) {
                 div.innerHTML = row[column['prop']];
             }
         }
-
     }
 
     /**
@@ -126,5 +190,4 @@ var tableFactory = function (element, config) {
     initialize_();
     return lightTable;
 };
-
-window.lightTable = tableFactory;
+window['lightTable'] = tableFactory;
