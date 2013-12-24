32493ea6a0b48249582edd7a2cc49c328dd986a6
Viktor Kukurba <victor.kukurba@sethq.com>
2013-12-24 23:00:46 +0200

    crossdomain jsonp example

---
+++ js/js-constructions/jsonp.js
@@ -0,0 +1,20 @@
+/**
+ * Created by viktor on 24.12.13.
+ */
+
+var showResponse = function(data){
+    console.log(data);
+}
+window.onload = function(){
+    //jQuery ajax cross domain request (jsonp)
+    $.ajax({
+        type: 'GET',
+        url: 'http://api.openweathermap.org/data/2.5/weather?q=London,uk',
+        dataType: 'jsonp',
+        success: showResponse
+    });
+    //javascript ajax cross domain request (jsonp)
+    var script = document.createElement('script');
+    script.src = "http://api.openweathermap.org/data/2.5/weather?q=Lviv,ua&callback=showResponse";
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
